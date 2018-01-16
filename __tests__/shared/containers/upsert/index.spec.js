a57c93bd983422b346e379580976e723e55d5da0
Kris Hedstrom <kris@nomadhealth.com>
2018-01-16 10:26:02 -0500

    fix tests

---
+++ __tests__/shared/containers/upsert/index.spec.js
@@ -0,0 +1,43 @@
+import React from "react";
+import { reduxForm } from "redux-form";
+import { createStore } from "redux";
+import { Provider } from "react-redux";
+import renderer from "react-test-renderer";
+
+import { Upsert } from "shared/containers/upsert";
+
+const spy = jest.fn();
+const store = createStore(() => ({}));
+
+const Decorated = reduxForm({ form: "testForm" })(Upsert);
+
+describe("Create snapshot", () => {
+  it("should render and match the snapshot for adding an item", () => {
+    const tree = renderer
+      .create(
+        <Provider store={store}>
+          <Decorated onSubmit={spy} />
+        </Provider>
+      )
+      .toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+});
+
+describe("Create snapshot", () => {
+  it("should render and match the snapshot for editing an item", () => {
+    const tree = renderer
+      .create(
+        <Provider store={store}>
+          <Decorated
+            onSubmit={spy}
+            initialValues={{ date: "July 12, 2008", amount: 10 }}
+            id={5}
+            fetchItem={spy}
+          />
+        </Provider>
+      )
+      .toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+});
