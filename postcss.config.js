86f965b47c8a0805e4a41af2d5edcbca8456c18a
Kristoffer Hedstrom <kris.hedstrom@gmail.com>
2018-01-21 11:18:11 -0500

    closer to production

--- postcss.config.js
+++ postcss.config.js
@@ -1,7 +1,21 @@
-module.exports = {
+module.exports = ({ env }) => ({
   plugins: {
     autoprefixer: {
       remove: false
-    }
+    },
+    // prettier-ignore
+    cssnano:
+      env === "production"
+        ? {
+          autoprefixer: false, // CSSNano's autoprefixer just removes prefixes.
+          reduceIdents: false, // Don't rename/shorten animation names.
+          mergeRules: false,
+          discardUnused: false, // Don't remove "unused" at-rules
+          discardComments: {
+            removeAll: true
+          },
+          zindex: false // Don't rebase z-index values.
+        }
+        : false
   }
-};
+});
