2bf0ebd0d6ae2d19d7e396131ff91a8f5aac6cfd
Kristoffer Hedstrom <kris.hedstrom@gmail.com>
2018-06-02 16:08:33 -0400

    Move app to shared root

--- src/client/scripts/app.js
+++ src/client/scripts/app.js
@@ -5,7 +5,7 @@ import { Provider } from "react-redux";
 
 import "client/styles/app";
 
-import App from "shared/components";
+import App from "shared/index";
 import { store } from "shared/data";
 
 const pageWrapper = document.getElementById("app-root");
