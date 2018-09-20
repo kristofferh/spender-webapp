bd2758212ffbc018ec1b39930616e34ca9e64b48
Kristoffer Hedstrom <kris.hedstrom@gmail.com>
2018-09-20 12:09:29 -0400

    Prettier updates

--- src/shared/features/verify/index.js
+++ src/shared/features/verify/index.js
@@ -66,4 +66,7 @@ const mapStateToProps = state => {
   return { ...verifyToken, userToken: user.token };
 };
 
-export default connect(mapStateToProps, { verifyToken, userLoggedIn })(Verify);
+export default connect(
+  mapStateToProps,
+  { verifyToken, userLoggedIn }
+)(Verify);
