e6006908e1becbc864071eb2e3f2b594e10f9cfd
Kristoffer Hedstrom <kris.hedstrom@gmail.com>
2019-01-23 16:55:50 -0500

    More stuff

--- src/shared/components/label/index.js
+++ src/shared/components/label/index.js
@@ -1,12 +1,23 @@
 import React from "react";
 import PropTypes from "prop-types";
 
-import { Container } from "./styles";
+import { Container, Remove } from "./styles";
 
-const Label = ({ children }) => <Container>{children}</Container>;
+const Label = ({ children, isRemovable, removeCallback }) => (
+  <Container>
+    <span>{children}</span>
+    {isRemovable && <Remove onClick={removeCallback}>x</Remove>}
+  </Container>
+);
+
+Label.defaultProps = {
+  removeCallback: () => {}
+};
 
 Label.propTypes = {
-  children: PropTypes.any
+  children: PropTypes.any,
+  isRemovable: PropTypes.bool,
+  removeCallback: PropTypes.func
 };
 
 export default Label;
--- src/shared/components/label/styles/index.js
+++ src/shared/components/label/styles/index.js
@@ -3,6 +3,9 @@ import styled from "@emotion/styled";
 import { fontSizeCaptionTwo, white, darkGray } from "shared/utils/styles";
 
 export const Container = styled.span`
+  position: relative;
+  display: inline-flex;
+  align-items: center;
   text-transform: uppercase;
   background: ${({ bgColor }) => (bgColor ? bgColor : darkGray)};
   padding: 4px 7px 5px;
@@ -11,4 +14,8 @@ export const Container = styled.span`
   margin-right: 4px;
   font-size: ${fontSizeCaptionTwo};
   font-weight: bold;
+`;
+
+export const Remove = styled.span`
+  margin-left: 4px;
 `;
