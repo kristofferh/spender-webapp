c1dabc716c9e080be27a9cf8731edc0683991a7c
Kristoffer Hedstrom <kris.hedstrom@gmail.com>
2019-05-05 11:15:27 +0200

    Typescript (#166)
    * Update babel-loader
    
    * Update @babel packages
    
    * Adding typescript packages and setting up new commands
    
    * Add tsconfig file
    
    * Add typings for react, start converting things
    
    * TS fixes
    
    * Tests and fixing paths

---
+++ src/shared/components/form-controls/action-buttons/index.tsx
@@ -0,0 +1,28 @@
+import React, { ReactNode } from "react";
+
+import { Container, PrimaryBtn, TextLink } from "./styles";
+
+export interface ActionButtonsProps {
+  primaryActionLabel?: ReactNode;
+  secondaryAction?: boolean;
+  secondaryActionLabel?: ReactNode;
+  secondaryActionCallback?: () => void;
+}
+
+const ActionButtons = ({
+  primaryActionLabel = "Submit",
+  secondaryAction,
+  secondaryActionCallback,
+  secondaryActionLabel
+}: ActionButtonsProps) => (
+  <Container>
+    {secondaryAction && (
+      <TextLink onClick={secondaryActionCallback}>
+        {secondaryActionLabel}
+      </TextLink>
+    )}{" "}
+    <PrimaryBtn type="submit">{primaryActionLabel}</PrimaryBtn>
+  </Container>
+);
+
+export default ActionButtons;
