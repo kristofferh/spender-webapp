03fc371df3f42e369f683e9f24eedb70d103624f
Kristoffer Hedstrom <kris.hedstrom@gmail.com>
2019-05-23 17:33:57 -0400

    Fix-up (#180)

--- __stories__/index.js
+++ __stories__/index.js
@@ -1,11 +1,6 @@
-import React from 'react';
-import { storiesOf } from '@storybook/react';
-import { Button } from '@storybook/react/demo';
+import React from "react";
+import { storiesOf } from "@storybook/react";
 
-storiesOf('Button', module)
-  .add('with text', () => (
-    <Button>Hello Button</Button>
-  ))
-  .add('with emoji', () => (
-    <Button><span role="img" aria-label="so cool">😀 😎 👍 💯</span></Button>
-  ));   
+import Loader from "../src/shared/components/loader";
+
+storiesOf("Loader", module).add("Loader", () => <Loader />);
