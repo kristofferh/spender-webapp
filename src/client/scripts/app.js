5facd5831829a327cc0f026c9cd79009e8b02a71
Kristoffer Hedstrom <kris.hedstrom@gmail.com>
2019-06-01 15:53:01 +0200

    Even more package updates (#183)
    * Update react-router-dom to 5, remove querystring and react-router-redux
    
    * Update code to not rely on react-router-redux
    
    * Don't require ID if it doesn't exist

--- src/client/scripts/app.js
+++ src/client/scripts/app.js
@@ -1,9 +1,7 @@
 import React from "react";
 import { render } from "react-dom";
 import { Provider } from "react-redux";
-import { ConnectedRouter } from "react-router-redux";
-import createHistory from "history/createBrowserHistory";
-import { Route } from "react-router-dom";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 
 import App from "shared/index";
 import { store } from "shared/data";
@@ -11,14 +9,13 @@ import { store } from "shared/data";
 import "client/styles/fonts/tiempos";
 import "client/styles/app";
 
-const history = createHistory();
 const pageWrapper = document.getElementById("app-root");
 
 render(
   <Provider store={store}>
-    <ConnectedRouter history={history}>
+    <Router>
       <Route component={App} />
-    </ConnectedRouter>
+    </Router>
   </Provider>,
   pageWrapper
 );
