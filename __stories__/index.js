d05f3d06b4f722a526ea75953638b85bc823a7a1
Kristoffer Hedstrom <kris.hedstrom@gmail.com>
2019-06-09 19:13:26 +0200

    Load storybook .ts/x files

--- __stories__/index.js
+++ __stories__/index.js
@@ -1,6 +1,12 @@
 import React from "react";
 import { storiesOf } from "@storybook/react";
 
+import ActionButtons from "../src/shared/components/form-controls/action-buttons";
+import { Input } from "../src/shared/components/form-controls/input";
 import Loader from "../src/shared/components/loader";
+
+storiesOf("Form Controls", module)
+  .add("Action Buttons", () => <ActionButtons />)
+  .add("Input", () => <Input />);
 
 storiesOf("Loader", module).add("Loader", () => <Loader />);
