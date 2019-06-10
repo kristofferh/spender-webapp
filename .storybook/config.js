141890ecec4e05eccd63ca5b45e588ed54e8f526
Kristoffer Hedstrom <kris.hedstrom@gmail.com>
2019-06-10 18:37:04 +0200

    Storybook addons (#189)
    * Cleanup
    
    * Updating snapshots
    
    * Update storybook
    
    * Separate form wrapper styles into own file
    
    * Load storybook .ts/x files
    
    * Add knobs
    
    * Jest addon

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,8 +1,18 @@
-import { configure } from "@storybook/react";
+import { configure, addDecorator } from "@storybook/react";
+import { withKnobs } from "@storybook/addon-knobs";
+import { withTests } from "@storybook/addon-jest";
+
+import results from "../jest-test-results.json";
 import "./styles.css";
 
 function loadStories() {
   require("../__stories__/index.js");
 }
 
+addDecorator(withKnobs);
+addDecorator(
+  withTests({
+    results
+  })
+);
 configure(loadStories, module);
