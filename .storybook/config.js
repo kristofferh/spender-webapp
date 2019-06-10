6922e808c13c784a17de8006f24f7ffaba527fbc
Kristoffer Hedstrom <kris.hedstrom@gmail.com>
2019-06-10 18:11:30 +0200

    Add knobs

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,8 +1,10 @@
-import { configure } from "@storybook/react";
+import { configure, addDecorator } from "@storybook/react";
+import { withKnobs } from "@storybook/addon-knobs";
 import "./styles.css";
 
 function loadStories() {
   require("../__stories__/index.js");
 }
 
+addDecorator(withKnobs);
 configure(loadStories, module);
--- __stories__/index.js
+++ __stories__/index.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { boolean, text } from "@storybook/addon-knobs";
 import { storiesOf } from "@storybook/react";
 
 import ActionButtons from "../src/shared/components/form-controls/action-buttons";
@@ -6,7 +7,18 @@ import { Input } from "../src/shared/components/form-controls/input";
 import Loader from "../src/shared/components/loader";
 
 storiesOf("Form Controls", module)
-  .add("Action Buttons", () => <ActionButtons />)
+  .add("Action Buttons", () => {
+    const primaryActionLabel = text("primaryActionLabel", "Send");
+    const secondaryAction = boolean("secondaryAction", false);
+    const secondaryActionLabel = text("seconaryActionLabel", "Cancel");
+    return (
+      <ActionButtons
+        secondaryAction={secondaryAction}
+        secondaryActionLabel={secondaryActionLabel}
+        primaryActionLabel={primaryActionLabel}
+      />
+    );
+  })
   .add("Input", () => <Input />);
 
 storiesOf("Loader", module).add("Loader", () => <Loader />);
